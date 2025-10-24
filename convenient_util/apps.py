966b8d9681bada7ac0f21d283e3a5332a50394ce
821152809 <158283689+821152809@users.noreply.github.com>
2025-10-24 20:46:49 +0800

    Delete convenient_util directory

--- convenient_util/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class ConvenientUtilConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "convenient_util"
-    verbose_name = "便捷工具"
